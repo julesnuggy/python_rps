671948ca012742ec87f336c8e09c30ffde41c019
julesnuggy <julesnuggy@gmail.com>
2021-07-10 14:10:37 +0100

    Refactor handling of user input validity

--- rps/Game.py
+++ rps/Game.py
@@ -3,23 +3,46 @@ from Enums import MoveEnum
 
 class Game:
     is_game_running = True
-    p1_move = None
-    p2_move = None
-    winner = None
+    p1_move = ''
+    p2_move = ''
+    winner = ''
 
     def end_game(self):
         self.is_game_running = False
 
     def set_p1_move(self, move):
-        self.p1_move = MoveEnum(move).name
+        if not(self.is_valid_input(move)):
+            print('Not a valid move!')
+            self.print_instructions()
+            return
+
+        self.p1_move = MoveEnum(int(move)).name
 
     def set_p2_move(self, move):
-        self.p2_move = MoveEnum(move).name
+        if not(self.is_valid_input(move)):
+            print('Not a valid move!')
+            self.print_instructions()
+            return
+
+        self.p2_move = MoveEnum(int(move)).name
 
     def set_winner(self, winner):
         self.winner = winner
 
     @staticmethod
-    def is_valid_move(move):
-        valid_moves = set(str(move.value) for move in MoveEnum)
-        return move in valid_moves
+    def is_valid_input(user_input):
+        valid_inputs = set(str(move.value) for move in MoveEnum)
+        return user_input in valid_inputs
+
+    @staticmethod
+    def is_valid_move(user_move):
+        valid_moves = set(move.name for move in MoveEnum)
+        return user_move in valid_moves
+
+    @staticmethod
+    def print_instructions():
+        print("""\
+-----------------------------------------
+| ROCK = 1 || PAPER = 2 || SCISSORS = 3 |
+-----------------------------------------
+        """)
--- rps/main.py
+++ rps/main.py
@@ -8,54 +8,30 @@ def start_game():
     print('Welcome to Rock Paper Scissors!')
 
     print('Player 1, what is your name?')
-    p1_name = str(input())
-    p1 = Player(p1_name)
+    p1 = Player(str(input()))
 
     print('Player 2, what is your name?')
-    p2_name = str(input())
-    p2 = Player(p2_name)
+    p2 = Player(str(input()))
 
     print(p1.name + ' VS ' + p2.name)
     print('FIGHT!')
-    print_instructions()
+    game.print_instructions()
 
     while game.is_game_running:
-        print(p1.name + ' make your move:')
-        p1_input = input()
 
-        while not(game.is_valid_move(p1_input)):
-            print('Not a valid move!')
-            print_instructions()
+        while not (game.is_valid_move(game.p1_move)):
             print(p1.name + ' make your move:')
-            p1_input = input()
+            game.set_p1_move(input())
 
-        game.set_p1_move(int(p1_input))
-
-        print(p2.name + ' make your move:')
-        p2_input = input()
-
-        while not(game.is_valid_move(p2_input)):
-            print('Not a valid move!')
-            print_instructions()
+        while not (game.is_valid_move(game.p2_move)):
             print(p2.name + ' make your move:')
-            p2_input = input()
-
-        game.set_p2_move(int(p2_input))
+            game.set_p2_move(input())
 
         print(p1.name + ' played ' + game.p1_move + ' and ' + p2.name + ' played ' + game.p2_move)
 
         print('Continue playing (y/n)?')
-        should_continue = str(input()).lower().strip()
-        if should_continue == 'n':
+        if str(input()).lower().strip() == 'n':
             game.end_game()
-
-
-def print_instructions():
-    print("""\
------------------------------------------
-| ROCK = 1 || PAPER = 2 || SCISSORS = 3 |
------------------------------------------
-    """)
 
 
 if __name__ == '__main__':
